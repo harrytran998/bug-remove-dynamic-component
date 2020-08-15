7819c4d2b8a1b99e97a1cba683f820f5a301a85b
thaycacac <thaycacac@gmail.com>
2020-08-15 12:27:20 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template v-for="(child, index) in children">
+      <component
+        :is="child"
+        :key="index"
+        :index-form="index"
+        :age="ages[index]"
+        @deleteForm="deleteForm($event)"
+      />
+    </template>
+    <button @click="add()">Add Another</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Form,
+  },
+  created() {
+    const ageLength = this.ages.length
+    for (let i = 0; i < ageLength; i++) {
+      this.children.push(Form)
+    }
+    console.log(this.children)
+  },
+  data() {
+    return {
+      children: [Form],
+      ages: [1, 2, 5, 3, 4],
+    }
+  },
+  methods: {
+    add() {
+      this.children.push(Form)
+    },
+    deleteForm(indexForm) {
+      console.log(indexForm)
+      this.children.splice(indexForm, 1)
+    },
+  },
 }
 </script>
 
---
+++ src/components/Form.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-row>
+    <el-col :span="12">
+      <el-form
+        :model="tempAge"
+        ref="forms"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item
+          label="age"
+          prop="age"
+          :rules="[
+            { required: true, message: 'age is required' },
+            { type: 'number', message: 'age must be a number' },
+          ]"
+        >
+          <el-input
+            type="age"
+            v-model.number="tempAge.age"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </el-col>
+    <el-col :span="12">
+      <i class="el-icon-delete" @click="deleteForm"></i>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  created() {
+    this.tempAge.age = this.age
+  },
+  data() {
+    return {
+      tempAge: {
+        age: null,
+      },
+    }
+  },
+  methods: {
+    deleteForm() {
+      this.$emit('deleteForm', this.indexForm)
+      console.log(this.indexForm)
+    },
+  },
+  props: {
+    age: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    indexForm: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
